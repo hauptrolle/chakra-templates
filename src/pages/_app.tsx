1e9429a73c2aaea92c440c662253464d5fc2ebc8
Achim Rolle <achimrolle@googlemail.com>
2020-12-28 20:01:31 +0100

    feature: add nprogress for loading indicator

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import type { AppProps } from "next/app";
+import Router from "next/router";
 import { ChakraProvider } from "@chakra-ui/react";
+import NProgress from "nprogress";
 
 import { theme } from "../theme";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function App({ Component, pageProps }: AppProps) {
   return (
--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,3 +1,20 @@
 import { extendTheme } from "@chakra-ui/react";
 
-export const theme = extendTheme({});
+export const theme = extendTheme({
+  styles: {
+    global: {
+      "#nprogress": {
+        pointerEvents: "none",
+      },
+      "#nprogress .bar": {
+        background: "green.200",
+        position: "fixed",
+        zIndex: "1031",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "2px",
+      },
+    },
+  },
+});
