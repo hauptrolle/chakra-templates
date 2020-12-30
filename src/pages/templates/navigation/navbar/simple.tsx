e5b918ac0b328a4b398d5dbb568d58e8cafa9e91
Achim Rolle <achimrolle@googlemail.com>
2020-12-30 18:08:48 +0100

    feature: finish simple navbar

--- src/pages/templates/navigation/navbar/simple.tsx
+++ src/pages/templates/navigation/navbar/simple.tsx
@@ -12,8 +12,10 @@ import {
   MenuList,
   MenuItem,
   MenuDivider,
+  useDisclosure,
+  Stack,
 } from "@chakra-ui/react";
-import { HamburgerIcon } from "@chakra-ui/icons";
+import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
 
 const Links = ["Dashboard", "Projects", "Team"];
 
@@ -22,7 +24,7 @@ const NavLink = ({ children }: { children: ReactNode }) => (
     px={2}
     py={1}
     rounded={"md"}
-    _hover={{ textDecoration: "none", bg: "gray.300" }}
+    _hover={{ textDecoration: "none", bg: "gray.200" }}
     href={"#"}
   >
     {children}
@@ -30,51 +32,77 @@ const NavLink = ({ children }: { children: ReactNode }) => (
 );
 
 export default function Simple() {
-  return (
-    <Flex
-      bg={"gray.200"}
-      p={4}
-      alignItems={"center"}
-      justifyContent={"space-between"}
-    >
-      <IconButton
-        size={"sm"}
-        icon={<HamburgerIcon />}
-        aria-label={"Open Menu"}
-        display={{ md: "none" }}
-      />
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
-      <HStack spacing={8} alignItems={"center"}>
-        <Box>Logo</Box>
-        <HStack as={"nav"} spacing={4} display={{ base: "none", md: "flex" }}>
-          {Links.map((link) => (
-            <NavLink key={link}>{link}</NavLink>
-          ))}
-        </HStack>
-      </HStack>
-      <Flex alignItems={"center"}>
-        <Menu>
-          <MenuButton
-            as={Button}
-            rounded={"full"}
-            variant={"link"}
-            cursor={"pointer"}
-          >
-            <Avatar
-              size={"sm"}
-              src={
-                "https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
-              }
-            />
-          </MenuButton>
-          <MenuList>
-            <MenuItem>Link 1</MenuItem>
-            <MenuItem>Link 2</MenuItem>
-            <MenuDivider />
-            <MenuItem>Link 3</MenuItem>
-          </MenuList>
-        </Menu>
-      </Flex>
-    </Flex>
+  return (
+    <>
+      <Box
+        bg={"gray.100"}
+        px={4}
+        position={"fixed"}
+        top={0}
+        left={0}
+        w={"full"}
+      >
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <IconButton
+            size={"md"}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={"Open Menu"}
+            display={{ md: !isOpen ? "none" : "inherit" }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={"center"}>
+            <Box>Logo</Box>
+            <HStack
+              as={"nav"}
+              spacing={4}
+              display={{ base: "none", md: "flex" }}
+            >
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
+          <Flex alignItems={"center"}>
+            <Menu>
+              <MenuButton
+                as={Button}
+                rounded={"full"}
+                variant={"link"}
+                cursor={"pointer"}
+              >
+                <Avatar
+                  size={"sm"}
+                  src={
+                    "https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9"
+                  }
+                />
+              </MenuButton>
+              <MenuList>
+                <MenuItem>Link 1</MenuItem>
+                <MenuItem>Link 2</MenuItem>
+                <MenuDivider />
+                <MenuItem>Link 3</MenuItem>
+              </MenuList>
+            </Menu>
+          </Flex>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4}>
+            <Stack as={"nav"} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+
+      <Box p={4} mt={16}>
+        Main Content Here
+      </Box>
+    </>
   );
 }
