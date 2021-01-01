a07a74edca063dd5b5a006905f64304effacc352
Achim Rolle <achimrolle@googlemail.com>
2021-01-01 13:45:34 +0100

    feature: add smooth scrollBehavior to html element

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -3,6 +3,9 @@ import { extendTheme } from "@chakra-ui/react";
 export const theme = extendTheme({
   styles: {
     global: {
+      html: {
+        scrollBehavior: "smooth",
+      },
       "#nprogress": {
         pointerEvents: "none",
       },
