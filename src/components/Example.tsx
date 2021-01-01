9ec36acf47a137c5a43df740b1b2e11bb2cb1c89
Achim Rolle <achimrolle@googlemail.com>
2021-01-01 14:08:38 +0100

    feature: use tabs for example container

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,15 +1,18 @@
-import { useState } from "react";
 import {
   Box,
-  Button,
-  Flex,
   Heading,
   HStack,
   IconButton,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
 } from "@chakra-ui/react";
-import { toSentenceCase } from "../utils";
 import Link from "next/link";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
+
+import { toSentenceCase } from "../utils";
 import { ResizableFrame } from "./ResizableFrame";
 import { CodeSample } from "./CodeSample";
 
@@ -26,68 +29,52 @@ export const Example = ({
   category,
   subCategory,
 }: ExampleProps) => {
-  const [tabState, setTabState] = useState<"preview" | "code">("preview");
-
   return (
-    <Box key={template.name}>
-      <Flex alignItems={"center"} justify={"space-between"} mb={4}>
-        <Heading as={"h3"} color={"gray.700"} size={"sm"}>
-          {toSentenceCase(template.name.split(".")[0])}
-        </Heading>
-        <HStack spacing={4}>
-          <Button
-            size={"sm"}
-            bg={tabState === "preview" ? "teal.100" : undefined}
-            color={tabState === "preview" ? "teal.700" : undefined}
-            _hover={{
-              bg: tabState === "preview" ? "teal.100" : undefined,
-            }}
-            onClick={() => setTabState("preview")}
-          >
-            Preview
-          </Button>
-          <Button
-            size={"sm"}
-            bg={tabState === "code" ? "teal.100" : undefined}
-            color={tabState === "code" ? "teal.700" : undefined}
-            _hover={{
-              bg: tabState === "code" ? "teal.100" : undefined,
-            }}
-            onClick={() => setTabState("code")}
-          >
-            Code
-          </Button>
-          <Link
-            href={`/templates/${category}/${subCategory}/${
-              template.name.split(".")[0]
-            }`}
-            passHref
-          >
-            <IconButton
-              as={"a"}
-              cursor={"pointer"}
-              icon={<ExternalLinkIcon />}
-              size={"sm"}
-              aria-label={"Open in Fullscreen"}
-              title={"Open in Fullscreen"}
-              target="_blank"
+    <Box key={template.name} bg={"gray.100"} padding={4} rounded={"md"}>
+      <Tabs
+        variant="soft-rounded"
+        colorScheme="teal"
+        justify={"space-between"}
+        size={"sm"}
+      >
+        <TabList alignItems={"center"} justifyContent={"space-between"}>
+          <Heading as={"h3"} color={"gray.700"} size={"sm"}>
+            {toSentenceCase(template.name.split(".")[0])}
+          </Heading>
+          <HStack spacing={4}>
+            <Tab>Preview</Tab>
+            <Tab>Code</Tab>
+            <Link
+              href={`/templates/${category}/${subCategory}/${
+                template.name.split(".")[0]
+              }`}
+              passHref
+            >
+              <IconButton
+                as={"a"}
+                cursor={"pointer"}
+                icon={<ExternalLinkIcon />}
+                size={"sm"}
+                aria-label={"Open in Fullscreen"}
+                title={"Open in Fullscreen"}
+                target="_blank"
+              />
+            </Link>
+          </HStack>
+        </TabList>
+        <TabPanels boxShadow={"xl"}>
+          <TabPanel px={0} pb={0}>
+            <ResizableFrame
+              src={`/templates/${category}/${subCategory}/${
+                template.name.split(".")[0]
+              }`}
             />
-          </Link>
-        </HStack>
-      </Flex>
-
-      <Box boxShadow={"xl"}>
-        <Box display={tabState === "preview" ? "block" : "none"}>
-          <ResizableFrame
-            src={`/templates/${category}/${subCategory}/${
-              template.name.split(".")[0]
-            }`}
-          />
-        </Box>
-        <Box display={tabState === "code" ? "block" : "none"}>
-          <CodeSample code={code} />
-        </Box>
-      </Box>
+          </TabPanel>
+          <TabPanel px={0} pb={0}>
+            <CodeSample code={code} />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </Box>
   );
 };
