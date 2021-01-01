8fc3a90727cbecae90110e3a17aa70a027a238b8
Achim Rolle <achimrolle@googlemail.com>
2021-01-01 17:37:36 +0100

    fix: small adjustments on hero with image text and cta

--- src/pages/templates/pageSections/hero/imageWithTextAndCTA.tsx
+++ src/pages/templates/pageSections/hero/imageWithTextAndCTA.tsx
@@ -11,12 +11,13 @@ export default function ImageWithTextAndCTA() {
   const heroHeight = useBreakpointValue({ base: "250px", md: "400px" });
   const ctaSize = useBreakpointValue({ base: "md", md: "lg" });
   const heroTextSize = useBreakpointValue({ base: "2xl", md: "4xl" });
-  const heroTextPadding = useBreakpointValue({ base: "1rem", md: "3.5rem" });
+  const heroTextPaddingX = useBreakpointValue({ base: "1rem", md: "3.5rem" });
 
   return (
-    <Box w={"100vw"} pos={"relative"} h={heroHeight}>
+    <Box w={"full"} pos={"relative"} h={heroHeight}>
       <Image
         src={"https://placeimg.com/1000/480/tech/grayscale"}
+        alt={"hero image"}
         w={"100%"}
         h={"100%"}
         objectFit={"cover"}
@@ -27,22 +28,21 @@ export default function ImageWithTextAndCTA() {
         top={"0"}
         h={"100%"}
         justifyContent={"center"}
-        spacing={"1.5rem"}
-        pl={heroTextPadding}
+        spacing={6}
+        px={heroTextPaddingX}
         bg={
           "linear-gradient(0.25turn, rgba(49, 130, 206, 0.7),rgba(49, 130, 206, 0.6), rgba(0,0,0,0))"
         }
+        maxW={{ base: "sm", sm: "md", md: "lg" }}
       >
         <Text
           color={"white"}
-          fontWeight={"bold"}
-          as={"p"}
-          lineHeight={"1.2"}
+          fontWeight={700}
+          lineHeight={1.2}
           fontSize={heroTextSize}
         >
-          Lorem ipsum dolor sit amet
-          <br /> consectetur adipiscing elit <br />
-          sed do eiusmod tempor
+          Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod
+          tempor
         </Text>
         <Button colorScheme={"orange"} size={ctaSize}>
           Call to action text
