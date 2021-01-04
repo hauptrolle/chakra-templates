a605560ac0a463530c5dca54aac039a33e9ec9e3
Lazar Nikolov <lazar.nikolov.94@gmail.com>
2021-01-04 20:33:33 +0100

    feature: Adding a Join our Discord server toast

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
 export const TEMPLATE_DIR = "src/pages/templates";
+export const DISCORD_INVITE_LINK = "https://discord.gg/CfUTeTw89G";
--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,6 +1,9 @@
 import { extendTheme } from "@chakra-ui/react";
 
 export const theme = extendTheme({
+  colors: {
+    discord: "#7289da",
+  },
   shadows: {
     largeSoft: "rgba(60, 64, 67, 0.15) 0px 2px 10px 6px;",
   },
