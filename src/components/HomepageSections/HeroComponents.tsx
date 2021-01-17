9f665244530720627f9641290c14ed2a46a26a3c
Lazar Nikolov <lazar@codechem.com>
2021-01-17 16:53:54 +0100

    feature: added hero components in placeholders

--- src/components/HomepageSections/HeroComponents.tsx
+++ src/components/HomepageSections/HeroComponents.tsx
@@ -1,11 +1,10 @@
 import { useTransform, useViewportScroll } from 'framer-motion';
-import { useBreakpointValue } from '@chakra-ui/react';
+import { useBreakpointValue, Image } from '@chakra-ui/react';
 
 import MotionBox from '@/components/motion/MotionBox';
 
 export const HeroComponents = () => {
   const isMobile = useBreakpointValue({ base: true, lg: false });
-  const heights = useBreakpointValue([100, 215, 332]) ?? 100;
   const { scrollYProgress } = useViewportScroll();
 
   const offsetFirst = useTransform(scrollYProgress, [0, 1], [0, -500]);
@@ -33,30 +32,37 @@ export const HeroComponents = () => {
   return (
     <MotionBox
       w={{ base: '100%', md: 384 }}
-      h={424}
+      h={295}
+      mb={20}
       initial={'initial'}
       animate={'show'}
       display={'flex'}
       flexDirection={'column'}
       alignItems={'center'}
+      position={'relative'}
       transition={{ staggerChildren: 0.15 }}>
       {[0, 1, 2, 3].map((i) => (
         <MotionBox
           key={i}
           style={{ translateY: isMobile ? 0 : offsets[i] }}
           opacity={0}
+          position={'absolute'}
+          bottom={0}
+          mb={3 - i * 20}
           variants={cardAnimationVariants}
           justify={'center'}
           flex={1}
-          p={4}
-          mt={i === 0 ? 0 : { base: heights * -3.5, sm: heights * -1 }}
+          p={12}
           bg={'white'}
           rounded={'3xl'}
           boxShadow={'xl'}
-          h={heights}
           w={{ base: `${100 - i * 10}%`, sm: widths[i] }}
-          zIndex={4 - i}
-        />
+          zIndex={4 - i}>
+          <Image
+            alt={'Component Placeholder'}
+            src={`/images/component-${i + 1}.svg`}
+          />
+        </MotionBox>
       ))}
     </MotionBox>
   );
