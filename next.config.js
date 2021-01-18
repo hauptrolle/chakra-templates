7d3168ebef023c925bcc688e96d235a7a293a50f
Achim Rolle <achimrolle@googlemail.com>
2021-01-18 08:40:07 +0100

    fix: update image domains in next config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = {
       'avatars0.githubusercontent.com',
       'avatars1.githubusercontent.com',
       'avatars2.githubusercontent.com',
+      'avatars3.githubusercontent.com',
       'avatars4.githubusercontent.com',
       'avatars5.githubusercontent.com',
       'avatars6.githubusercontent.com',
