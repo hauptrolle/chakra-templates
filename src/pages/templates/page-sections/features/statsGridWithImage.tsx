eadcb845e753e9f81d47455dafd6fac235bef314
Lazar Nikolov <lazar.nikolov.94@gmail.com>
2021-01-18 19:09:30 +0100

    fix: refactored Stats Grid with Image template layout

--- src/pages/templates/page-sections/features/statsGridWithImage.tsx
+++ src/pages/templates/page-sections/features/statsGridWithImage.tsx
@@ -11,65 +11,77 @@ import {
 
 export default function StatsGridWithImage() {
   return (
-    <Stack
-      minH={'100vh'}
-      bg={'gray.800'}
-      direction={{ base: 'column', md: 'row' }}>
-      <Stack flex={1} color={'gray.400'} justify={{ lg: 'center' }}>
-        <Container maxW={'3xl'} py={8}>
-          <Box mb={{ base: 8, md: 20 }}>
-            <Text
-              fontFamily={'heading'}
-              fontWeight={700}
-              textTransform={'uppercase'}
-              mb={3}
-              fontSize={'xl'}>
-              Technology
-            </Text>
-            <Heading
-              color={'white'}
-              mb={4}
-              fontSize={{ base: '3xl', md: '5xl' }}>
-              21st century agriculture
-            </Heading>
-            <Text fontSize={'xl'}>
-              The NewLife™ technology allows you to monitor your crops and get
-              complete insights at real time. The proprietary software/hardware
-              ecosystem prevents your plants from getting neglected.
-            </Text>
-          </Box>
-
-          <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
-            {stats.map((stat) => (
-              <Box key={stat.title}>
-                <Text
-                  fontFamily={'heading'}
-                  fontSize={'3xl'}
-                  color={'white'}
-                  mb={3}>
-                  {stat.title}
-                </Text>
-                <Text fontSize={'xl'}>{stat.content}</Text>
-              </Box>
-            ))}
-          </SimpleGrid>
-        </Container>
-      </Stack>
-
+    <Box bg={'gray.800'} position={'relative'}>
       <Flex
         flex={1}
-        display={{ base: 'none', md: 'flex' }}
+        zIndex={0}
+        display={{ base: 'none', lg: 'flex' }}
         backgroundImage="url('/templates/stats-grid-with-image.png')"
+        backgroundSize={'cover'}
         backgroundPosition="center"
         backgroundRepeat="no-repeat"
-        position={'relative'}>
+        position={'absolute'}
+        width={'50%'}
+        insetY={0}
+        right={0}>
         <Flex
           bgGradient={'linear(to-r, gray.800 10%, transparent)'}
           w={'full'}
           h={'full'}
         />
       </Flex>
-    </Stack>
+      <Container maxW={'7xl'} zIndex={10} position={'relative'}>
+        <Stack direction={{ base: 'column', lg: 'row' }}>
+          <Stack
+            flex={1}
+            color={'gray.400'}
+            justify={{ lg: 'center' }}
+            py={{ base: 4, md: 20, xl: 60 }}>
+            <Box mb={{ base: 8, md: 20 }}>
+              <Text
+                fontFamily={'heading'}
+                fontWeight={700}
+                textTransform={'uppercase'}
+                mb={3}
+                fontSize={'xl'}
+                color={'gray.500'}>
+                Technology
+              </Text>
+              <Heading
+                color={'white'}
+                mb={5}
+                fontSize={{ base: '3xl', md: '5xl' }}>
+                21st century agriculture
+              </Heading>
+              <Text fontSize={'xl'} color={'gray.400'}>
+                The NewLife™ technology allows you to monitor your crops and get
+                complete insights at real time. The proprietary
+                software/hardware ecosystem prevents your plants from getting
+                neglected.
+              </Text>
+            </Box>
+
+            <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+              {stats.map((stat) => (
+                <Box key={stat.title}>
+                  <Text
+                    fontFamily={'heading'}
+                    fontSize={'3xl'}
+                    color={'white'}
+                    mb={3}>
+                    {stat.title}
+                  </Text>
+                  <Text fontSize={'xl'} color={'gray.400'}>
+                    {stat.content}
+                  </Text>
+                </Box>
+              ))}
+            </SimpleGrid>
+          </Stack>
+          <Flex flex={1} />
+        </Stack>
+      </Container>
+    </Box>
   );
 }
 
