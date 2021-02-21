ee9bc5edaf9d94fee6cdf21ce98a5849e46925ab
Achim Rolle <achimrolle@googlemail.com>
2021-02-21 11:07:08 +0100

    feature: add testimonial templates

---
+++ src/data/templates/testimonials.ts
@@ -0,0 +1,16 @@
+import { SubCategory } from '../types';
+
+export const testimonials: SubCategory = {
+  name: 'Testimonials',
+  id: 'testimonials',
+  children: [
+    {
+      name: 'With Speech Bubbles',
+      filename: 'withSpeechBubbles',
+    },
+    {
+      name: 'With large Quote',
+      filename: 'withLargeQuote',
+    },
+  ],
+};
