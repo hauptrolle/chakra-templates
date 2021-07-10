d85ff8ace40c1b4122f7a5792659fc333f172f7e
Faheem Patel <faheem@faheempatel.com>
2021-07-11 00:31:38 +0100

    fix: Stretched image
    Without 'object-fit: cover' the image appeared stretched and awkward

--- src/pages/templates/page-sections/features/splitWithImage.tsx
+++ src/pages/templates/page-sections/features/splitWithImage.tsx
@@ -96,6 +96,7 @@ export default function SplitWithImage() {
             src={
               'https://images.unsplash.com/photo-1554200876-56c2f25224fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
             }
+            objectFit={'cover'}
           />
         </Flex>
       </SimpleGrid>
