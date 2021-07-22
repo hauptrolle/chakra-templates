db387d200d8f1a522d592f593caed8c09e9eeb0c
marco <marcon@hyperting.com>
2021-07-22 14:03:11 +0200

    refactor: updated framer-motion usage after framer-motion upgrade

--- src/components/motion/MotionBox.tsx
+++ src/components/motion/MotionBox.tsx
@@ -3,7 +3,7 @@ import { Box, forwardRef } from '@chakra-ui/react';
 import { motion, isValidMotionProp } from 'framer-motion';
 
 // TODO check typings
-const MotionBox = motion.custom(
+const MotionBox = motion(
   forwardRef((props, ref) => {
     const chakraProps = Object.fromEntries(
       Object.entries(props).filter(([key]) => !isValidMotionProp(key))
