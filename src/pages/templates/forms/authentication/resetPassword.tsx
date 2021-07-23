a5e5303db1a192da9a60cbae0225d6fac4dda532
Brock Tillotson <brock.tillotson@castlerockits.com>
2021-07-23 10:06:41 -0700

    feature: reset password and forgot password forms

---
+++ src/pages/templates/forms/authentication/resetPassword.tsx
@@ -0,0 +1,55 @@
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  useColorModeValue,
+} from '@chakra-ui/react';
+
+export default function ResetPasswordForm(): JSX.Element {
+  return (
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}>
+      <Stack
+        spacing={4}
+        w={'full'}
+        maxW={'md'}
+        bg={useColorModeValue('white', 'gray.700')}
+        rounded={'xl'}
+        boxShadow={'lg'}
+        p={6}>
+        <Heading lineHeight={1.1} fontSize={{ base: '2xl', md: '3xl' }}>
+          Enter new password
+        </Heading>
+        <FormControl id="email" isRequired>
+          <FormLabel>Email address</FormLabel>
+          <Input
+            placeholder="your-email@example.com"
+            _placeholder={{ color: 'gray.500' }}
+            type="email"
+          />
+        </FormControl>
+        <FormControl id="password" isRequired>
+          <FormLabel>Password</FormLabel>
+          <Input type="password" />
+        </FormControl>
+        <Stack spacing={6}>
+          <Button
+            bg={'blue.400'}
+            color={'white'}
+            _hover={{
+              bg: 'blue.500',
+            }}>
+            Submit
+          </Button>
+        </Stack>
+      </Stack>
+    </Flex>
+  );
+}
