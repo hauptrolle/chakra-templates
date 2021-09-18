7a4436d18ae5d6c54bbe68129fa5b87e222069e9
eleven12 <maddinenichenchunaidu@gmail.com>
2021-09-19 00:28:17 +0530

    Added cookies prefrences simple card

---
+++ src/data/templates/cookie-preferences.ts
@@ -0,0 +1,13 @@
+import { SubCategory } from 'data/types';
+import React from 'react';
+
+export const cookiePreferences: SubCategory = {
+  name: 'Cookie Preference',
+  id: 'cookiePreferences',
+  children: [
+    {
+      name: 'Simple Cookies prefrence',
+      filename: 'simpleCookiePreference',
+    },
+  ],
+};
