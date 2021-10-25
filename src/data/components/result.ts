9c29e310f2c2a87b9e4fd96ebfbe694ca375c1fb
Mohamed Samara <45723974+mohamedsamara@users.noreply.github.com>
2021-10-24 17:54:30 -0700

    feat: build success result component

---
+++ src/data/components/result.ts
@@ -0,0 +1,12 @@
+import { SubCategory } from '../types';
+
+export const result: SubCategory = {
+  name: 'Result',
+  id: 'result',
+  children: [
+    {
+      name: 'Success result',
+      filename: 'success',
+    },
+  ],
+};
