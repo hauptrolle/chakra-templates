ad795285f377f7d3df30015463ff04da03ca8eb9
Edwin Lunandy <populationtwo@gmail.com>
2021-12-09 21:37:05 -0500

    feat: product details page

--- src/data/templates/product.ts
+++ src/data/templates/product.ts
@@ -1,12 +1,12 @@
 import { SubCategory } from '../types';
 
 export const product: SubCategory = {
-  name: 'Product',
-  id: 'product',
+  name: 'Product Details',
+  id: 'productDetails',
   children: [
     {
-      name: 'Default',
-      filename: 'defaultProduct',
+      name: 'Simple',
+      filename: 'simple',
     },
   ],
 };
