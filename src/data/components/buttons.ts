ac6bcd5c357444f49c1e00a12c78c422b8cf85d7
Danish Shakeel <danishshakeel54@gmail.com>
2022-03-07 22:36:46 +0530

    feat(button): add general purpose buttons
    Currently, Chakra Templates has no general purpose buttons.
    
    Buttons need to be extracted from other templates and components.

---
+++ src/data/components/buttons.ts
@@ -0,0 +1,20 @@
+import { SubCategory } from '../types';
+
+export const buttons: SubCategory = {
+  name: 'Buttons',
+  id: 'buttons',
+  children: [
+    {
+      name: 'Color Mode Toggle',
+      filename: 'colorModeToggle',
+    },
+    {
+      name: 'Secondary Options with Menu',
+      filename: 'secondaryOptions',
+    },
+    {
+      name: 'Button with Tabbed Popover',
+      filename: 'buttonTabbedPopover',
+    },
+  ],
+};
