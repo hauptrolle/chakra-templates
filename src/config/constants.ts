1eab1ed3c0401dafac052418a88ae718090e9047
Luciano Alves <luas10c@gmail.com>
2023-07-08 15:03:33 -0300

    chore: move constants to folder config

---
+++ src/config/constants.ts
@@ -0,0 +1,39 @@
+export const TEMPLATE_DIR = 'src/pages/templates'
+export const PRODUCTION_URL = 'https://chakra-templates.dev'
+
+/**
+ * LINKS
+ */
+export const BUY_ME_A_COFFEE_LINK = 'https://www.buymeacoffee.com/hauptrolle'
+export const TWITTER_LINK_ACHIM = 'https://twitter.com/hauptrolle_'
+export const TEMPLATES_LINK = '/page-sections/hero'
+export const DISCORD_INVITE_LINK = 'https://discord.gg/4Y3bQk6sTb'
+export const GITHUB_LINK = 'https://github.com/hauptrolle/chakra-templates'
+export const ANALYTICS_LINK = 'https://app.splitbee.io/public/chakra-templates.dev'
+export const TWITTER_LINK = 'https://twitter.com/ChakraTemplates'
+export const FIGMA_LINK =
+  'https://www.figma.com/community/file/935959613109017886/Chakra-Templates-UI-Kit'
+export const CONTRIBUTORS_LINK =
+  'https://github.com/hauptrolle/chakra-templates/graphs/contributors'
+export const CONTRIBUTE_LINK =
+  'https://github.com/hauptrolle/chakra-templates/blob/main/CONTRIBUTING.md'
+export const SUGGEST_TEMPLATE_LINK =
+  'https://github.com/hauptrolle/chakra-templates/issues/new?assignees=&labels=&template=template_request.md'
+
+/**
+ * Splitbee events
+ */
+export const SPLITBEE_HERO_SUGGEST_TEMPLATE = 'hero_click_suggest_template'
+export const SPLITBEE_TEMPLATE_CLICK_COPY = 'template_copy_button'
+export const SPLITBEE_TEMPLATE_MANUAL_COPY = 'template_copy_manual'
+
+/**
+ * SEO constants
+ */
+export const SEO_TITLE = 'Chakra Templates'
+export const SEO_DESCRIPTION =
+  'A growing open-source collection of hand-crafted Chakra UI templates ready to drop into your React project.'
+export const SEO_KEYWORDS =
+  'chakra ui, ui templates, web templates, web design, react templates, react ui templates, chakra ui templates, chakra templates'
+export const SEO_OG_IMAGE = 'https://chakra-templates.dev/favicon.png'
+export const SEO_TWITTER_IMAGE = 'https://chakra-templates.dev/favicon.png'
