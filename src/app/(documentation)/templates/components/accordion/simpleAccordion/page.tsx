df09e193467daf12d3b6af90d8c950d1757c0328
Luciano Alves <luas10c@gmail.com>
2023-07-08 15:30:47 -0300

    fix: panel colors
    Co-authored-by: Fernando Unger <fdasilvaunger@gmail.com>

--- src/app/(documentation)/templates/components/accordion/simpleAccordion/page.tsx
+++ src/app/(documentation)/templates/components/accordion/simpleAccordion/page.tsx
@@ -29,11 +29,13 @@ export default function SimpleAccordion() {
               justifyContent="space-between"
               p={4}
               _hover={{ bg: 'gray.100' }}>
-              <Text fontSize="md">What is Chakra UI?</Text>
+              <Text fontSize="md" color="gray.800">
+                What is Chakra UI?
+              </Text>
               <ChevronDownIcon fontSize="24px" />
             </AccordionButton>
             <AccordionPanel pb={4}>
-              <Text>
+              <Text color="gray.600">
                 Chakra UI is a simple and modular component library that gives developers
                 the building blocks they need to create web applications.
               </Text>
@@ -45,12 +47,13 @@ export default function SimpleAccordion() {
               alignItems="center"
               justifyContent="space-between"
               p={4}
-              _hover={{ bg: 'gray.100' }}>
+              _hover={{ bg: 'gray.100' }}
+              color="gray.800">
               <Text fontSize="md">What advantages to use?</Text>
               <ChevronDownIcon fontSize="24px" />
             </AccordionButton>
             <AccordionPanel pb={4}>
-              <Text>
+              <Text color="gray.600">
                 Chakra UI offers a variety of advantages including ease of use,
                 accessibility, and customization options. It also provides a comprehensive
                 set of UI components and is fully compatible with React.
@@ -63,12 +66,13 @@ export default function SimpleAccordion() {
               alignItems="center"
               justifyContent="space-between"
               p={4}
-              _hover={{ bg: 'gray.100' }}>
+              _hover={{ bg: 'gray.100' }}
+              color="gray.800">
               <Text fontSize="md">How to start using Chakra UI?</Text>
               <ChevronDownIcon fontSize="24px" />
             </AccordionButton>
             <AccordionPanel pb={4}>
-              <Text>
+              <Text color="gray.600">
                 To get started with Chakra UI, you can install it via npm or yarn, and
                 then import the components you need in your project. The Chakra UI
                 documentation is also a great resource for getting started and learning
