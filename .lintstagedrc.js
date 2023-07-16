0a01dc6a9c2c2243092bc21d96873e19f0592a79
Luciano Alves <luas10c@gmail.com>
2023-07-16 20:24:20 -0300

    chore: update lint-staged script

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
 /** @type{import('lint-staged').Config}*/
 module.exports = {
-  '**/*.{ts,tsx}': ["eslint --config .eslintrc.js --ext .ts --ext .tsx --fix"]
+  '**/*.{js,ts,tsx}': ["eslint . --config .eslintrc.js --ext .js,.ts,.tsx --fix"]
 }
