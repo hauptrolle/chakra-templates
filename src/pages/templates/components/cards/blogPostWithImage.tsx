3d5f331f1a6a69d265b7ce8c0aadcf67407c99a3
7zip <mario@halicea.com>
2023-07-17 22:10:13 +0200

    FTR: Used Chakra Image instead of Next, and removed fixed height so the image can shrink when decreasing width of the viewport

--- src/pages/templates/components/cards/blogPostWithImage.tsx
+++ src/pages/templates/components/cards/blogPostWithImage.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import {
   Box,
   Center,
@@ -7,6 +6,7 @@ import {
   Stack,
   Avatar,
   useColorModeValue,
+  Image,
 } from '@chakra-ui/react';
 
 export default function blogPostWithImage() {
@@ -20,18 +20,11 @@ export default function blogPostWithImage() {
         rounded={'md'}
         p={6}
         overflow={'hidden'}>
-        <Box
-          h={'210px'}
-          bg={'gray.100'}
-          mt={-6}
-          mx={-6}
-          mb={6}
-          pos={'relative'}>
+        <Box bg={'gray.100'} mt={-6} mx={-6} mb={6} pos={'relative'}>
           <Image
             src={
               'https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
             }
-            layout={'fill'}
           />
         </Box>
         <Stack>
